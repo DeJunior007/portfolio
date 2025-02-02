3a5644655200b3c6e9304a967a8125ee78f080be
DeJunior007 <deiltonp74@gmail.com>
2025-02-02 20:39:16 -0300

    Enhance Navbar and Footer components with dark mode support and updated styling

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="bg-gray-800 text-white py-4">
-            <div className="container mx-auto text-center">
-                <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-[#271300] dark:bg-primary-light text-white dark:text-primary-dark py-4">
+      <div className="container mx-auto text-center">
+        <a
+          href="https://www.linkedin.com/in/deilton-pedro"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          &copy; {new Date().getFullYear()} Deilton Pedro Junior. All rights
+          reserved.
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- app/components/navbar/index.js
+++ app/components/navbar/index.js
@@ -1,20 +1,78 @@
-import React from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
+import { Transition } from "@headlessui/react";
+import { navbarItens } from "./config/arrays";
+import { LuSun } from "react-icons/lu";
+import { LuMoonStar } from "react-icons/lu";
+import { GiAbstract020 } from "react-icons/gi";
 
 const Navbar = () => {
-    return (
-        <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="text-white text-lg font-bold">My Portfolio</div>
-                <div className="space-x-4">
-                    <a href="#home" className="text-gray-300 hover:text-white">Home</a>
-                    <a href="#about" className="text-gray-300 hover:text-white">About</a>
-                    <a href="#projects" className="text-gray-300 hover:text-white">Projects</a>
-                    <a href="#contact" className="text-gray-300 hover:text-white">Contact</a>
-                </div>
-            </div>
-        </nav>
-    );
+  const [isShowing, setIsShowing] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedMode = localStorage.getItem("darkMode") === "true";
+    setDarkMode(savedMode);
+    if (savedMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    // Start the animation after component mounts
+    setIsShowing(true);
+  }, []);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    localStorage.setItem("darkMode", !darkMode);
+    if (!darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  return (
+    <Transition
+      show={isShowing}
+      enter="transition-all duration-700 ease-out"
+      enterFrom="-translate-y-full"
+      enterTo="translate-y-0"
+      leave="transition-all duration-700 ease-in"
+      leaveFrom="translate-y-0"
+      leaveTo="-translate-y-full"
+    >
+      <nav className="fixed w-full p-4 border-b border-[#6F4E37] dark:border-primary-light bg-primary-light dark:bg-primary-dark z-10">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-[#724E2C] dark:text-primary-light font-bold flex items-center gap-1">
+            <GiAbstract020 className="size-5" /> Portfolio
+          </div>
+          <div className="space-x-6 flex items-center">
+            {navbarItens.map((item, index) => (
+              <a
+                key={index}
+                href={item.href}
+                className="group transition duration-300 text-aux-light font-semibold text-sm dark:text-primary-light dark:hover:text-feature hover:text-feature"
+              >
+                {item.title}
+                <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 dark:bg-primary-light bg-[#724E2C]"></span>
+              </a>
+            ))}
+            <button
+              onClick={toggleDarkMode}
+              className="p-1 rounded-md bg-[#724E2C] hover:bg-primary-dark hover:dark:bg-primary-light dark:bg-[#724E2C]  ease-in-out transition-all"
+            >
+              {darkMode ? (
+                <LuSun className="text-aux-light" />
+              ) : (
+                <LuMoonStar className="text-aux-dark" />
+              )}
+            </button>
+          </div>
+        </div>
+      </nav>
+    </Transition>
+  );
 };
 
 export default Navbar;
-
